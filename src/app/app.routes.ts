d4cff4b276aad263c097633e6579b1925902e4d7
FLimeGreen <fynn.wegen@aol.com>
2026-06-15 15:54:47 +0200

    Routing implementiert.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ResearchView } from './Research/research-view/research-view';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "research", component: ResearchView },
+  { path: "", redirectTo: "research", pathMatch: "full" }
+];
