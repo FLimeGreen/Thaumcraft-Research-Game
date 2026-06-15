5e745cad73b78760227e4171dd139e39cc1906d9
FLimeGreen <fynn.wegen@aol.com>
2026-06-15 16:08:51 +0200

    Nicht funktionierende 404

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { ResearchView } from './Research/research-view/research-view';
+import { NotFound } from './not-found/not-found';
 
 export const routes: Routes = [
-  { path: "research", component: ResearchView },
-  { path: "", redirectTo: "research", pathMatch: "full" }
+  { path: 'research', component: ResearchView },
+  { path: '', redirectTo: 'research', pathMatch: 'full' },
+  { path: '**', component: NotFound }
 ];
