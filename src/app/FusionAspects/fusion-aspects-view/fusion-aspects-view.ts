95a62f617085783f5045654e12ccfc30f4c023a4
FLimeGreen <fynn.wegen@aol.com>
2026-06-22 15:05:18 +0200

    Import standart Ref.

--- src/app/FusionAspects/fusion-aspects-view/fusion-aspects-view.ts
+++ src/app/FusionAspects/fusion-aspects-view/fusion-aspects-view.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ResearchInventoryField } from '../../Research/research-inventory-field/research-inventory-field';
+import { GoldRankDisplay } from '../../gold-rank-display/gold-rank-display';
 
 @Component({
   selector: 'app-fusion-aspects-view',
-  imports: [],
+  imports: [GoldRankDisplay, ResearchInventoryField],
   templateUrl: './fusion-aspects-view.html',
   styleUrl: './fusion-aspects-view.css',
 })
-export class FusionAspectsView {}
+export class FusionAspectsView { }
