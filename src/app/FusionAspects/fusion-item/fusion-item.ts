c7bd5a27f83baf8f65f7cfe2825555836288abad
FLimeGreen <fynn.wegen@aol.com>
2026-06-22 16:26:24 +0200

    Dif in Selection funktioniert.

--- src/app/FusionAspects/fusion-item/fusion-item.ts
+++ src/app/FusionAspects/fusion-item/fusion-item.ts
@@ -2,6 +2,8 @@ import { Component, Input, signal } from '@angular/core';
 import { ResearchInventory } from '../../../Services/Inventory_Aspects/research-inventory';
 import { sign } from 'crypto';
 import { ResearchInventoryField } from '../../Research/research-inventory-field/research-inventory-field';
+import { ResearchFieldService } from '../../../Services/Research_Field/research-field-Service';
+import { EnLevel } from '../../../Model/Aspects/Enum_Level';
 
 @Component({
   selector: 'app-fusion-item',
@@ -17,7 +19,7 @@ export class FusionItem {
   public menge = signal(0);
 
 
-  constructor(private researchInventory: ResearchInventory) {
+  constructor(private researchInventory: ResearchInventory, private selected: ResearchFieldService) {
     this.researchInventory.inventorySelectionChanged.subscribe((selection: string) => {
       // Hier kannst du die internen Funktion aufrufen
       console.log("Inventory Selection Changed", selection);
@@ -29,6 +31,23 @@ export class FusionItem {
   }
 
   update_View() {
+
+    // Leere Selection
+    if (this.selected.SelectedAspectInventory === null) {
+      this.title.set("Nothing Selected.");
+      this.IMG.set("");
+      this.menge.set(0);
+      return;
+    }
+
+    // Primal Element
+    if (this.selected.SelectedAspectInventory.Level === EnLevel.Primal) {
+      this.title.set("Primal does not work.");
+      this.IMG.set("");
+      this.menge.set(0);
+      return;
+    }
+
     if (this.dis_typ === 'firstComponent') {
       this.title.set("First");
     }
