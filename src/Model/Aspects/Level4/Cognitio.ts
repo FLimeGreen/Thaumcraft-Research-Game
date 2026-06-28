19816d90b5edbf633edd5f80f23014d3d8118622
FLimeGreen <fynn.wegen@aol.com>
2026-06-28 18:54:45 +0200

    Level4

--- src/Model/Aspects/Level4/Cognitio.ts
+++ src/Model/Aspects/Level4/Cognitio.ts
@@ -1,22 +1,22 @@
 import { Aspect } from "../Aspect";
 import { EnLevel } from "../Enum_Level";
+import { Spiritus } from "../Level3/Spiritus";
 import { Ignis } from "../Primal/Ignis";
-import { Perditio } from "../Primal/Perditio";
 
-export class Gelum extends Aspect {
+export class Cognitio extends Aspect {
   constructor(ver_emthy: boolean = false) {
 
-    const path_img: string = "/assets/svgs/Gelum.svg";
+    const path_img: string = "/assets/svgs/Cognitio.svg";
 
     if (ver_emthy) {
-      super(EnLevel.Tier1, path_img, [], []);
+      super(EnLevel.Tier3, path_img, [], []);
     } else {
-      super(EnLevel.Tier1, path_img, [
+      super(EnLevel.Tier3, path_img, [
         new Ignis(true),
-        new Perditio(true)
+        new Spiritus(true)
       ], [
         new Ignis(true),
-        new Perditio(true)
+        new Spiritus(true)
       ]);
     }
   }
