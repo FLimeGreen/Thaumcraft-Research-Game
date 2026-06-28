e2f223d73667055eed3906245d6e134fa6868bcd
FLimeGreen <fynn.wegen@aol.com>
2026-06-28 21:24:18 +0200

    Changes bis to Inventory Service

--- src/Model/Aspects/ListofallAspects.ts
+++ src/Model/Aspects/ListofallAspects.ts
@@ -16,47 +16,161 @@ import { Vacuos } from '../../Model/Aspects/Level1/Vacuos';
 import { Vitreus } from '../../Model/Aspects/Level1/Vitreus';
 import { Victus } from '../../Model/Aspects/Level1/Victus';
 import { Venenum } from '../../Model/Aspects/Level1/Venenum';
+import { Bestia } from './Level2/Bestia';
+import { Fames } from './Level2/Fames';
+import { Herba } from './Level2/Herba';
+import { Iter } from './Level2/Iter';
+import { Limus } from './Level2/Limus';
+import { Metallum } from './Level2/Metallum';
+import { Mortuus } from './Level2/Mortuus';
+import { Praecantatio } from './Level2/Praecantatio';
+import { Sano } from './Level2/Sano';
+import { Tenebrae } from './Level2/Tenebrae';
+import { Vinculum } from './Level2/Vinculum';
+import { Volatus } from './Level2/Volatus';
+import { Alienis } from './Level3/Alienis';
+import { Arbor } from './Level3/Arbor';
+import { Auram } from './Level3/Auram';
+import { Corpus } from './Level3/Corpus';
+import { Exanimis } from './Level3/Exanimis';
+import { Spiritus } from './Level3/Spiritus';
+import { Vitium } from './Level3/Vitium';
+import { Cognitio } from './Level4/Cognitio';
+import { Sensus } from './Level4/Sensus';
+import { Humanus } from './Level5/Humanus';
+import { Instrumentum } from './Level6/Instrumentum';
+import { Lucrum } from './Level6/Lucrum';
+import { Messis } from './Level6/Messis';
+import { Perfodio } from './Level6/Perfodio';
+import { Fabrico } from './Level7/Fabrico';
+import { Machina } from './Level7/Machina';
+import { Meto } from './Level7/Meto';
+import { Pannus } from './Level7/Pannus';
+import { Telum } from './Level7/Telum';
+import { Tutamen } from './Level7/Tutamen';
 
 export class ListofallAspects {
 
-    public static readonly ListOfAllAspects: Aspect[] = [
-        new Aer(), 
-        new Aqua(), 
-        new Ignis(), 
-        new Ordo(), 
-        new Perditio(), 
-        new Terra(),
-        new Gelum(), 
-        new Lux(), 
-        new Motus(), 
-        new Permutatio(), 
-        new Potentia(), 
-        new Tempestas(),
-        new Vacuos(),
-        new Venenum(),
-        new Victus(),
-        new Vitreus()
-    ];
+  public static readonly ListOfAllAspects: Aspect[] = [
+    new Aer(),
+    new Aqua(),
+    new Ignis(),
+    new Ordo(),
+    new Perditio(),
+    new Terra(),
+    new Gelum(),
+    new Lux(),
+    new Motus(),
+    new Permutatio(),
+    new Potentia(),
+    new Tempestas(),
+    new Vacuos(),
+    new Venenum(),
+    new Victus(),
+    new Vitreus(),
+    new Bestia,
+    new Fames(),
+    new Herba(),
+    new Iter(),
+    new Limus(),
+    new Metallum(),
+    new Mortuus(),
+    new Praecantatio(),
+    new Sano(),
+    new Tenebrae(),
+    new Vinculum(),
+    new Volatus(),
+    new Alienis(),
+    new Arbor(),
+    new Auram(),
+    new Corpus(),
+    new Exanimis(),
+    new Spiritus(),
+    new Vitium(),
+    new Cognitio(),
+    new Sensus(),
+    new Humanus(),
+    new Instrumentum(),
+    new Lucrum(),
+    new Messis(),
+    new Perfodio(),
+    new Fabrico(),
+    new Machina(),
+    new Meto(),
+    new Pannus(),
+    new Telum(),
+    new Tutamen()
+  ];
 
-    public static readonly ListOfAspectsPrimal: Aspect[] = [
-        new Aer(), 
-        new Aqua(), 
-        new Ignis(), 
-        new Ordo(), 
-        new Perditio(), 
-        new Terra()
-    ];
+  public static readonly ListOfAspectsPrimal: Aspect[] = [
+    new Aer(),
+    new Aqua(),
+    new Ignis(),
+    new Ordo(),
+    new Perditio(),
+    new Terra()
+  ];
 
-    public static readonly ListOfAspectsLevel1: Aspect[] = [
-        new Gelum(), 
-        new Lux(), 
-        new Motus(), 
-        new Permutatio(), 
-        new Potentia(), 
-        new Tempestas(),
-        new Vacuos(),
-        new Venenum(),
-        new Victus(),
-        new Vitreus()
-    ];
+  public static readonly ListOfAspectsLevel1: Aspect[] = [
+    new Gelum(),
+    new Lux(),
+    new Motus(),
+    new Permutatio(),
+    new Potentia(),
+    new Tempestas(),
+    new Vacuos(),
+    new Venenum(),
+    new Victus(),
+    new Vitreus()
+  ];
+
+  public static readonly ListOfAspectsLevel2: Aspect[] = [
+    new Bestia,
+    new Fames(),
+    new Herba(),
+    new Iter(),
+    new Limus(),
+    new Metallum(),
+    new Mortuus(),
+    new Praecantatio(),
+    new Sano(),
+    new Tenebrae(),
+    new Vinculum(),
+    new Volatus()
+  ];
+
+  public static readonly ListOfAspectsLevel3: Aspect[] = [
+    new Alienis(),
+    new Arbor(),
+    new Auram(),
+    new Corpus(),
+    new Exanimis(),
+    new Spiritus(),
+    new Vitium()
+  ];
+
+  public static readonly ListOfAspectsLevel4: Aspect[] = [
+    new Cognitio(),
+    new Sensus()
+  ];
+
+  public static readonly ListOfAspectsLevel5: Aspect[] = [
+    new Humanus()
+  ];
+
+  public static readonly ListOfAspectsLevel6: Aspect[] = [
+    new Instrumentum(),
+    new Lucrum(),
+    new Messis(),
+    new Perfodio()
+  ];
+
+  public static readonly ListOfAspectsLevel7: Aspect[] = [
+    new Fabrico(),
+    new Machina(),
+    new Meto(),
+    new Pannus(),
+    new Telum(),
+    new Tutamen()
+  ];
 }
--- src/Services/Inventory_Aspects/research-inventory.ts
+++ src/Services/Inventory_Aspects/research-inventory.ts
@@ -16,6 +16,38 @@ import { Vacuos } from '../../Model/Aspects/Level1/Vacuos';
 import { Vitreus } from '../../Model/Aspects/Level1/Vitreus';
 import { Victus } from '../../Model/Aspects/Level1/Victus';
 import { Venenum } from '../../Model/Aspects/Level1/Venenum';
+import { Bestia } from '../../Model/Aspects/Level2/Bestia';
+import { Fames } from '../../Model/Aspects/Level2/Fames';
+import { Herba } from '../../Model/Aspects/Level2/Herba';
+import { Iter } from '../../Model/Aspects/Level2/Iter';
+import { Limus } from '../../Model/Aspects/Level2/Limus';
+import { Metallum } from '../../Model/Aspects/Level2/Metallum';
+import { Mortuus } from '../../Model/Aspects/Level2/Mortuus';
+import { Praecantatio } from '../../Model/Aspects/Level2/Praecantatio';
+import { Sano } from '../../Model/Aspects/Level2/Sano';
+import { Tenebrae } from '../../Model/Aspects/Level2/Tenebrae';
+import { Vinculum } from '../../Model/Aspects/Level2/Vinculum';
+import { Volatus } from '../../Model/Aspects/Level2/Volatus';
+import { Alienis } from '../../Model/Aspects/Level3/Alienis';
+import { Arbor } from '../../Model/Aspects/Level3/Arbor';
+import { Auram } from '../../Model/Aspects/Level3/Auram';
+import { Corpus } from '../../Model/Aspects/Level3/Corpus';
+import { Exanimis } from '../../Model/Aspects/Level3/Exanimis';
+import { Spiritus } from '../../Model/Aspects/Level3/Spiritus';
+import { Vitium } from '../../Model/Aspects/Level3/Vitium';
+import { Cognitio } from '../../Model/Aspects/Level4/Cognitio';
+import { Sensus } from '../../Model/Aspects/Level4/Sensus';
+import { Humanus } from '../../Model/Aspects/Level5/Humanus';
+import { Instrumentum } from '../../Model/Aspects/Level6/Instrumentum';
+import { Lucrum } from '../../Model/Aspects/Level6/Lucrum';
+import { Messis } from '../../Model/Aspects/Level6/Messis';
+import { Perfodio } from '../../Model/Aspects/Level6/Perfodio';
+import { Fabrico } from '../../Model/Aspects/Level7/Fabrico';
+import { Machina } from '../../Model/Aspects/Level7/Machina';
+import { Meto } from '../../Model/Aspects/Level7/Meto';
+import { Pannus } from '../../Model/Aspects/Level7/Pannus';
+import { Telum } from '../../Model/Aspects/Level7/Telum';
+import { Tutamen } from '../../Model/Aspects/Level7/Tutamen';
 
 @Injectable({
   providedIn: 'root',
@@ -51,11 +83,84 @@ export class ResearchInventory {
   public InventoryLevel1Signal = signal(this.ListOfAspectsInventoryLevel1);
 
 
+  public readonly ListOfAspectsInventoryLevel2: { aspect: Aspect, count: number }[] = [
+    { aspect: new Bestia(), count: 10 },
+    { aspect: new Fames(), count: 10 },
+    { aspect: new Herba(), count: 10 },
+    { aspect: new Iter(), count: 10 },
+    { aspect: new Limus(), count: 10 },
+    { aspect: new Metallum(), count: 10 },
+    { aspect: new Mortuus(), count: 10 },
+    { aspect: new Praecantatio(), count: 10 },
+    { aspect: new Sano(), count: 10 },
+    { aspect: new Tenebrae(), count: 10 },
+    { aspect: new Vinculum(), count: 10 },
+    { aspect: new Volatus(), count: 10 }
+  ];
+
+  public InventoryLevel2Signal = signal(this.ListOfAspectsInventoryLevel2);
+
+
+  public readonly ListOfAspectsInventoryLevel3: { aspect: Aspect, count: number }[] = [
+    { aspect: new Alienis(), count: 10 },
+    { aspect: new Arbor(), count: 10 },
+    { aspect: new Auram(), count: 10 },
+    { aspect: new Corpus(), count: 10 },
+    { aspect: new Exanimis(), count: 10 },
+    { aspect: new Spiritus(), count: 10 },
+    { aspect: new Vitium(), count: 10 }
+  ];
+
+  public InventoryLevel3Signal = signal(this.ListOfAspectsInventoryLevel3);
+
+
+  public readonly ListOfAspectsInventoryLevel4: { aspect: Aspect, count: number }[] = [
+    { aspect: new Cognitio(), count: 10 },
+    { aspect: new Sensus(), count: 10 }
+  ];
+
+  public InventoryLevel4Signal = signal(this.ListOfAspectsInventoryLevel4);
+
+
+  public readonly ListOfAspectsInventoryLevel5: { aspect: Aspect, count: number }[] = [
+    { aspect: new Humanus(), count: 10 }
+  ];
+
+  public InventoryLevel5Signal = signal(this.ListOfAspectsInventoryLevel5);
+
+
+  public readonly ListOfAspectsInventoryLevel6: { aspect: Aspect, count: number }[] = [
+    { aspect: new Instrumentum(), count: 10 },
+    { aspect: new Lucrum(), count: 10 },
+    { aspect: new Messis(), count: 10 },
+    { aspect: new Perfodio(), count: 10 }
+  ];
+
+  public InventoryLevel6Signal = signal(this.ListOfAspectsInventoryLevel6);
+
+
+  public readonly ListOfAspectsInventoryLevel7: { aspect: Aspect, count: number }[] = [
+    { aspect: new Fabrico(), count: 10 },
+    { aspect: new Machina(), count: 10 },
+    { aspect: new Meto(), count: 10 },
+    { aspect: new Pannus(), count: 10 },
+    { aspect: new Telum(), count: 10 },
+    { aspect: new Tutamen(), count: 10 }
+  ];
+
+  public InventoryLevel7Signal = signal(this.ListOfAspectsInventoryLevel7);
+
   public inventorySelectionChanged = new EventEmitter<string>();
 
   changed() {
     this.InventoryPrimalSignal.set([...this.ListOfAspectsInventoryPrimal]);
     this.InventoryLevel1Signal.set([...this.ListOfAspectsInventoryLevel1]);
+    this.InventoryLevel2Signal.set([...this.ListOfAspectsInventoryLevel2]);
+    this.InventoryLevel3Signal.set([...this.ListOfAspectsInventoryLevel3]);
+    this.InventoryLevel4Signal.set([...this.ListOfAspectsInventoryLevel4]);
+    this.InventoryLevel5Signal.set([...this.ListOfAspectsInventoryLevel5]);
+    this.InventoryLevel6Signal.set([...this.ListOfAspectsInventoryLevel6]);
+    this.InventoryLevel7Signal.set([...this.ListOfAspectsInventoryLevel7]);
   }
 
   getCount(aspect: Aspect): number {
